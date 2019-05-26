f7bf67c9e43f20bbdc769ca84a4616553bf05e51
shs6785 <mac@macui-MacBook-Pro.local>
2019-05-26 09:57:27 +0900

    Installing Pug

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,7 @@ import routes from "../routes";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req,res) => res.send("Videos"));
+videoRouter.get(routes.videos, (req,res) => res.render("Videos"));
 videoRouter.get(routes.upload, (req,res) => res.send("upload"));
 videoRouter.get(routes.videoDetail, (req,res) => res.send("videoDetail"));
 videoRouter.get(routes.editVideo, (req,res) => res.send("editVideo"));
