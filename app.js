7385f7d98f4172fda74a7afdd51549326c640507
shs6785 <mac@macui-MacBook-Pro.local>
2019-05-26 18:43:36 +0900

    Template Variables in pug

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ import userRouter from "./routers/userRouter"
 import videoRouter from "./routers/videoRouter"
 import globalRouter from "./routers/globalRouter"
 import routes from "./routes"
+import { localsMiddleware } from "./middleware";
 
 const app = express();
 
+
+app.use(helmet());
 app.set('view engine', 'pug')
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
 
+app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
--- init.js
+++ init.js
@@ -6,4 +6,3 @@ const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
-app.get("/test", (req, res) => res.send("테스트"))
